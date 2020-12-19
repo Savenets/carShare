872039b9d16a9a70acc7e668f3d5f29b67bae5ea
Nickolay Savenets <nickolay.savenets@stuzo.com>
2020-12-19 20:15:43 +0200

    added theme toggle and other

--- vehicle-share/src/App.vue
+++ vehicle-share/src/App.vue
@@ -1,36 +1,37 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+      <v-toolbar-title>Share your vehicle</v-toolbar-title>
+      <v-btn class="ma-2" outlined color="indigo" @click="toggleTheme" >change theme to {{ themeName }}</v-btn>
       <v-spacer></v-spacer>
       <v-btn
-              v-for="link in links"
-              :key="`${link.label}-header-link`"
-              text
-              rounded
-              :to="link.url"
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        text
+        rounded
+        :to="link.url"
       >
         {{ link.label }}
       </v-btn>
     </v-app-bar>
-    <v-content>
+    <v-main>
       <router-view></router-view>
-    </v-content>
+    </v-main>
     <v-footer color="primary lighten-1" padless>
       <v-layout justify-center wrap>
         <v-btn
-                v-for="link in links"
-                :key="`${link.label}-footer-link`"
-                color="white"
-                text
-                rounded
-                class="my-2"
-                :to="link.url"
+          v-for="link in links"
+          :key="`${link.label}-footer-link`"
+          color="white"
+          text
+          rounded
+          class="my-2"
+          :to="link.url"
         >
           {{ link.label }}
         </v-btn>
         <v-flex primary lighten-2 py-4 text-center white--text xs12>
-          {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+          <strong>{{ new Date().getFullYear() }}</strong>
         </v-flex>
       </v-layout>
     </v-footer>
@@ -42,24 +43,31 @@
     name: 'App',
     data() {
       return {
+        themeName: 'dark',
         links: [
           {
             label: 'Home',
             url: '/'
           },
+         /* {
+            label: 'Signup',
+            url: '/signup'
+          },*/
+          {
+            label: 'Dashboard',
+            url: '/dashboard'
+          },
           {
             label: 'Login',
             url: '/login'
           },
-          {
-            label: 'Signup',
-            url: '/signup'
-          },
-          {
-            label: 'Dashboard',
-            url: '/dashboard'
-          }
         ]
+      }
+    },
+    methods: {
+      toggleTheme() {
+        this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+        this.themeName = this.$vuetify.theme.isDark ? 'white' : 'dark';
       }
     }
   }
--- vehicle-share/src/plugins/vuetify.ts
+++ vehicle-share/src/plugins/vuetify.ts
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#bdb76b'
+      },
+      dark: {
+        primary: '#003333'
+      }
+    }
+  }
 });
