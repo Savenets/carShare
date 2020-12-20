5c3d801c72defd1289a530691e222ac5ddc84932
Nickolay Savenets <nickolay.savenets@stuzo.com>
2020-12-20 15:52:58 +0200

    some clean up

--- vehicle-share/src/components/Intro.vue
+++ vehicle-share/src/components/Intro.vue
@@ -10,6 +10,8 @@
         class="mb-5"
         cols="12"
       >
+        <v-btn x-large color="success" class="mr-6" dark> I want to share a vehicle</v-btn>
+        <v-btn x-large color="error" dark>I want to get my vehicle shared</v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -20,56 +22,7 @@
     name: 'Intro',
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+
     }),
   }
 </script>
